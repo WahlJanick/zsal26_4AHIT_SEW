bb2565bda8b344e1a8047879d5b9d145f46ab222
Janick Wahl <Janick.Wahl@htlwy.at>
2026-09-24 15:01:12 +0200

    forgot to push

---
+++ FigureList/FigureList/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Figur
+{
+    interface IFigur
+    {
+        double BerechneOberflaeche();
+        double BerechneVolumen();
+    }
+
+    abstract class Figur : IFigur
+    {
+        public Figur(string beschreibung)
+        {
+            Beschreibung = beschreibung;
+        }
+
+        public string Beschreibung { get; }
+
+        public abstract double BerechneOberflaeche();
+        public abstract double BerechneVolumen();
+    }
+
+    class Kugel : Figur
+    {
+        public double Radius { get; }
+
+        public Kugel(string beschreibung, double radius) : base(beschreibung)
+        {
+            Radius = radius;
+        }
+
+        public override double BerechneOberflaeche()
+        {
+            return 4.0 * Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public override double BerechneVolumen()
+        {
+            return (4.0 * Math.PI * Math.Pow(Radius, 3)) / 3.0;
+        }
+    }
+
+    class Wuerfel : Figur
+    {
+        public double Seitenlaenge { get; }
+
+        public Wuerfel(string beschreibung, double a) : base(beschreibung)
+        {
+            Seitenlaenge = a;
+        }
+
+        public override double BerechneOberflaeche()
+        {
+            return 6.0 * Math.Pow(Seitenlaenge, 2);
+        }
+
+        public override double BerechneVolumen()
+        {
+            return Math.Pow(Seitenlaenge, 3);
+        }
+    }
+
+    class FigurListe : List<Figur>
+    {
+        public FigurListe() { }
+
+        public void AusgabeAllerFiguren()
+        {
+            foreach (var figur in this)
+            {
+                Console.WriteLine($"Bezeichnung: {figur.Beschreibung}, Oberfläche: {figur.BerechneOberflaeche():F2}, Volumen: {figur.BerechneVolumen():F2}");
+            }
+        }
+
+        public void Remove(int i)
+        {
+            this.RemoveAt(i);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FigurListe liste = new FigurListe();
+
+            Kugel k1 = new Kugel("K1", 10);
+            liste.Add(new Kugel("K2", 8));
+            liste.Add(new Wuerfel("W1", 3));
+            liste.Add(new Wuerfel("W2", 4));
+            liste.Add(k1);
+            liste.Remove(1);
+            liste.Remove(k1);
+            Console.WriteLine($"Anzahl der Elemente in der Liste: {liste.Count}");
+
+            liste.AusgabeAllerFiguren();
+
+            Console.ReadKey();
+        }
+    }
+}
